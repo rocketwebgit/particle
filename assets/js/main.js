a54ce210d7f5a40e5e3fc0a76d04aa569e5035f7
scarletks1214 <scarlet.ks1214@gmail.com>
2019-10-13 23:08:30 +0300

    fixed contents

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () { new SweetScroll({}); particlesJS("particles-js", { particles: { number: { value: 30, density: { enable: !0, value_area: 800 } }, color: { value: "#ffffff" }, shape: { type: "polygon", stroke: { width: 0, color: "#000000" }, polygon: { nb_sides: 5 }, image: { src: "img/github.svg", width: 100, height: 100 } }, opacity: { value: .5, random: !1, anim: { enable: !1, speed: 1, opacity_min: .1, sync: !1 } }, size: { value: 3, random: !0, anim: { enable: !1, speed: 19.18081918081918, size_min: .1, sync: !1 } }, line_linked: { enable: !0, distance: 150, color: "#ffffff", opacity: .4, width: 1 }, move: { enable: !0, speed: 4, direction: "none", random: !0, straight: !1, out_mode: "out", bounce: !1, attract: { enable: !1, rotateX: 600, rotateY: 1200 } }, nb: 80 }, interactivity: { detect_on: "canvas", events: { onhover: { enable: !1, mode: "grab" }, onclick: { enable: !0, mode: "push" }, resize: !0 }, modes: { grab: { distance: 400, line_linked: { opacity: 1 } }, bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 }, repulse: { distance: 200, duration: .4 }, push: { particles_nb: 4 }, remove: { particles_nb: 2 } } }, retina_detect: !0 }) }, !1);
+document.addEventListener("DOMContentLoaded", function () { new SweetScroll({}); particlesJS("particles-js", {    "particles": {
+  "number": {
+      "value": 160,
+      "density": {
+          "enable": false
+      }
+  },
+  "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+          "speed": 4,
+          "size_min": 0.3
+      }
+  },
+  "line_linked": {
+      "enable": false
+  },
+  "move": {
+      "random": true,
+      "speed": 1,
+      "direction": "top",
+      "out_mode": "out"
+  }
+},
+"interactivity": {
+  "events": {
+      "onhover": {
+          "enable": true,
+          "mode": "bubble"
+      },
+      "onclick": {
+          "enable": true,
+          "mode": "repulse"
+      }
+  },
+  "modes": {
+      "bubble": {
+          "distance": 250,
+          "duration": 2,
+          "size": 0,
+          "opacity": 0
+      },
+      "repulse": {
+          "distance": 400,
+          "duration": 4
+      }
+  }
+}}) }, !1);
